d7df5b20118240a530976b63635ae94813509220
srikarankam <137189772+srikarankam@users.noreply.github.com>
2024-08-01 13:00:16 +0530

    Update main.tf

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -15,6 +15,7 @@ resource "aws_eks_cluster" "my_cluster" {
 resource "aws_eks_node_group" "my_node_group" {
 ami_id = "ami-068e0f1a600cd311c" 
 cluster_name    = aws_eks_cluster.my_cluster.name
+disk_size = "20"
 instance_types = ["t3.medium"]
 node_group_name = "my-_node_group"
   node_role_arn   = "arn:aws:iam::083211754961:role/all_access_uber"  # Replace with your IAM role ARN
