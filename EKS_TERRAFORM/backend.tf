e09fdd5bc9461796b01ac0ef6145d6f8a2af1775
srikarankam <137189772+srikarankam@users.noreply.github.com>
2024-08-01 12:00:23 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "srivcube" # Replace with your actual S3 bucket name
+    bucket = "sri-vcube" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
